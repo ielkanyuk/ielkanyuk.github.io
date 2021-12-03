f4594a025dd44f75fd816c9367637731eb670c3b
ielkanyuk <ielkanyuk@gmail.com>
2021-12-03 21:05:52 +0500

    Add logo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-const header = document.querySelector('.header');
-
-window.onscroll = () => {
-	if (window.scrollY > 50) {
-		header.classList.add('header_active');
-	} else {
-		header.classList.remove('header_active');
-	}
-}
-
 document.querySelectorAll('a[href^="#"]').forEach(link => {
 	link.addEventListener('click', function(e) {
 		e.preventDefault();
