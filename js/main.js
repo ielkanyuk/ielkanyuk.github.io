c73c526832d6c643ae4dae96c3d81eb03e5ec21e
ielkanyuk <ielkanyuk@gmail.com>
2021-12-03 11:42:35 +0500

    Scroll to sections, main subtitle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-(function () {
-	const header = document.querySelector('.header');
-	window.onscroll = () => {
-		if (window.scrollY > 50) {
-			header.classList.add('header_active');
-		} else {
-			header.classList.remove('header_active');
-		}
+const header = document.querySelector('.header');
+
+window.onscroll = () => {
+	if (window.scrollY > 50) {
+		header.classList.add('header_active');
+	} else {
+		header.classList.remove('header_active');
 	}
-}());
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+	link.addEventListener('click', function(e) {
+		e.preventDefault();
+
+		let href = this.getAttribute('href').substring(1);
+
+		const scrollTarget = document.getElementById(href);
+
+		window.scrollBy({
+			top: scrollTarget.getBoundingClientRect().top,
+			behavior: 'smooth'
+		});
+	});
+});
